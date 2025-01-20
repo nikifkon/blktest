ccf0d1cf3e705c895cb30311f8aec5a95a72b88d
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2025-01-20 18:21:10 +0500

    add plotter

---
+++ app/plot.py
@@ -0,0 +1,31 @@
+import subprocess
+
+
+def plot(results, out_file):
+    plot_script = ";".join(
+        [
+            "set xlabel 'iodepth'",
+            "set ylabel 'clat, ms'",
+            "set terminal png",
+            "set output '{}'".format(out_file.name),
+            "set style data histogram",
+            "set style fill solid 0.5 border -1",
+            "plot "
+            + ", ".join(
+                ["'-' using 2:xtic(1) title '{}'".format(test) for test in results]
+            ),
+        ]
+    )
+    stdin = "\n".join(
+        [
+            "\n".join(
+                [
+                    "{} {}".format(iodepth, clat_ns // 1_000_000)
+                    for iodepth, clat_ns in data
+                ]
+            )
+            + "\ne"
+            for data in results.values()
+        ]
+    )
+    subprocess.run(["gnuplot", "-e", plot_script], input=stdin.encode() * 2)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 from argparse import ArgumentParser, FileType
 from app.fio import measure
+from app.plot import plot
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,6 +21,8 @@ def main():
     args = parser.parse_args()
     logger.info(args)
     results = measure(args)
+    logger.info(results)
+    plot(results, args.output)
 
 
 if __name__ == "__main__":
